96e0ff360f496653c1fae742a1d11f78d6477a8a
Daniel Vinyas <danielvinyastorres@gmail.com>
2024-03-14 15:50:03 +0100

    chore: setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'next/core-web-vitals',
+    'react-app',
+    'react-app/jest',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  endOfLine: "auto",
+  bracketSpacing: true,
+  trailingComma: "none"
+}
